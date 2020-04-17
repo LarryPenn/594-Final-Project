8c39a3079883dd1f9f4e3833a013f32845fdbf2d
Holly Furnari <hfurnari@seas.upenn.edu>
2020-04-17 15:32:39 -0400

    test

--- 594 Final Project/src/edu/upenn/cit594/data/FinesData.java
+++ 594 Final Project/src/edu/upenn/cit594/data/FinesData.java
@@ -1,7 +1,9 @@
 package edu.upenn.cit594.data;
 
-public class FinesData {
+import java.util.HashMap;
 
+public class FinesData {
+	HashMap<Integer,Integer> finesByZipCode;
 	public FinesData() {
 		// TODO Auto-generated constructor stub
 	}
